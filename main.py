a40804a63d5f8ff8d0d2c390498060dc59468f54
Nishchay Sinha <nishchay.space@gmail.com>
2022-07-03 21:50:06 +0530

    THINGS HAPPENED

--- main.py
+++ main.py
@@ -2,6 +2,9 @@
 from selenium import webdriver
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 import platform
 
@@ -23,26 +26,24 @@ else:
 
 
 # Obtain the Google Map URL
-url = ["https://www.google.com/maps/place/\
-Papa+John's+Pizza/@40.7936551,-74.0124687,17z/data=!3m1!4b1!\
-4m5!3m4!1s0x89c2580eaa74451b:0x15d743e4f841e5ed!8m2!3d40.\
-7936551!4d-74.0124687", "https://www.google.com/maps/place/\
-Lucky+Dhaba/@30.653792,76.8165233,17z/data=!3m1!4b1!4m5!3m4!\
-1s0x390feb3e3de1a031:0x862036ab85567f75!8m2!3d30.653792!4d76.818712"]
-# Initialize variables and declare it 0
-i = 0
+url = "https://www.google.com/maps/@28.5123109,77.3799704,14z/data=!3m1!4b1!4m5!3m4!1s0x390ce88db913c975:0xbbec271e3ce71be1!8m2!3d28.5110359!4d77.3941312"
 
-# Create a loop for obtaining data from URLs
-for i in range(len(url)):
 
-	# Open the Google Map URL
-	browser.get(url[i])
+# Open the Google Map URL
+browser.get(url)
 
-	# Obtain the title of that place
-	title = browser.find_element(By.CLASS_NAME, "x3AX1-LfntMc-header-title-title")
-	print(i+1, "-", title.text)
 
-	# Obtain the ratings of that place
-	stars = browser.find_element(By.CLASS_NAME, "aMPvhf-fI6EEc-KVuj8d")
-	print("The stars of restaurant are:", stars.text)
-	print("\n")
+WebDriverWait(browser, 10000).until(EC.presence_of_element_located((By.CLASS_NAME, "lMbq3e")))
+
+
+# Obtain the title of that place
+title = browser.find_element(By.CLASS_NAME, "lMbq3e")
+print(title.text)
+
+'''
+# Obtain the ratings of that place
+stars = browser.find_element(By.CLASS_NAME, "aMPvhf-fI6EEc-KVuj8d")
+print("The stars of restaurant are:", stars.text)
+print("\n")
+'''
+
