1244eb1e1a3157da7d89158173b0f03d06262e5f
Nishchay Sinha <nishchay.space@gmail.com>
2022-07-04 13:52:48 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,41 +3,32 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
 import platform
 
-# Make browser open in background
-options = webdriver.ChromeOptions()
-options.add_argument('headless')
-
-# Create the webdriver object
-if platform.system()=="Windows":
-    browser = webdriver.Chrome(
-        executable_path="chromedriver.exe", options=options)
-
-elif platform.system()=="Darwin":
-    browser = webdriver.Chrome(
-        executable_path="/usr/local/bin/chromedriver", options=options)
-
-else:
-    print("Linux Detected, Currently Doesn't support Linux")
-
-lat="28.549819596259738"
-lng="77.3659236434599"
-
-lat=str(input("enter lat: "))
-lng=str(input("enter long: "))
-
-# Obtain the Google Map URL
-url = "http://maps.google.com/maps?z=12&t=m&q=loc:"+lat+"+"+lng
+def reverseGeocoder(lat,lng):
 
 
-# Open the Google Map URL
-browser.get(url)
+    # Make browser open in background
+    options = webdriver.ChromeOptions()
+    options.add_argument('headless')
 
+    # Create the webdriver object
+    if platform.system()=="Windows":
+        browser = webdriver.Chrome(
+            executable_path="chromedriver.exe", options=options)
 
-WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "LCF4w")))
+    elif platform.system()=="Darwin":
+        browser = webdriver.Chrome(
+            executable_path="/usr/local/bin/chromedriver", options=options)
 
-# Obtain the title of that place
-title = browser.find_element(By.CLASS_NAME, "LCF4w")
-print(title.text)
+    else:
+        print("Linux Detected, Currently Doesn't support Linux")
+
+    # Obtain the Google Map URL
+    url = "http://maps.google.com/maps?z=12&t=m&q=loc:"+lat+"+"+lng
+    # Open the Google Map URL
+    browser.get(url)
+    WebDriverWait(browser, 100).until(EC.presence_of_element_located((By.CLASS_NAME, "LCF4w")))
+    title = browser.find_element(By.CLASS_NAME, "LCF4w")
+    return title
+
